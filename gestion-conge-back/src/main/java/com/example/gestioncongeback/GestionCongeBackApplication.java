2f8e39acdbd48ad96cf77679d1d0d2bdb4c86701
Fanambony <ramarosonbenjamina@gmail.com>
2024-07-23 22:24:36 +0300

    back

---
+++ gestion-conge-back/src/main/java/com/example/gestioncongeback/GestionCongeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioncongeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCongeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCongeBackApplication.class, args);
+    }
+
+}
